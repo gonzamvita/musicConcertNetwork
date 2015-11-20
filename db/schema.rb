9e7b02cd23d5eba741983aeaed5506d2b7d159ab
gonzamvita <gonzamv@gmail.com>
2015-11-20 14:50:37 +0100

    In comments section beloooooooooow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120093327) do
+ActiveRecord::Schema.define(version: 20151120132039) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "concert_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["concert_id"], name: "index_comments_on_concert_id"
 
   create_table "concerts", force: :cascade do |t|
     t.string   "band"
